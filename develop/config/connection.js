4ee50a2c70d4ec0d68c288bf6971594f1b2fee19
JonathanHaws <jonathanmhaws1@gmail.com>
2023-03-21 01:11:08 -0600

    spent forever trying to find out why it wasnt working. enviroment variable DB not DATABASE for jawsdb_url

--- develop/config/connection.js
+++ develop/config/connection.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDATABASE_URL
-  ? new Sequelize(process.env.JAWSDATABASE_URL)
+const sequelize = process.env.JAWSDB_URL
+  ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DATABASE_NAME, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
       host: 'localhost',
       dialect: 'mysql',
