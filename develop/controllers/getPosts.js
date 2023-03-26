d89090318f8461c79b04d2ef7f2539d9aa07743c
JonathanHaws <jonathanmhaws1@gmail.com>
2023-03-26 17:08:23 -0600

    array map function is horrible with async request. for loops are the way to go and what I will be using from now on. hopefully this fixes the issue on the deployed site

--- develop/controllers/getPosts.js
+++ develop/controllers/getPosts.js
@@ -8,18 +8,18 @@ async function getPosts(req, userId) {
         posts = await Post.findAll({ include: [{ model: User, attributes: ['username'] },], raw: true,});
     }
     
-    posts.map(async (post) => { 
+    for (const post of posts) {
         post.user = req.session?.user;
         post.author = (await User.findByPk(post.userId)).username;
         post.canDelete = req.session?.user?.id === post.userId;
         post.date = new Date(post.createdAt).toLocaleDateString('en-US', { year:'numeric', month:'long', day:'numeric'}); 
         post.comments = await Comment.findAll({ where: { postId: post.id }, raw: true,});  //console.log(post.comments)  
-        post.comments.map(async (comment) => { 
+        for (const comment of post.comments) { 
             comment.author = (await User.findByPk(comment.userId)).username;
             comment.canDelete = req.session?.user?.id === comment.userId;
             comment.date = new Date(comment.createdAt).toLocaleDateString('en-US', { year:'numeric', month:'long', day:'numeric'}); 
-        });
-    }); 
+        };
+    };
 
     posts.reverse();
     return posts;
